c7253d33a7b5957a605bf8428129594270db03f2
John Ruiz <rubixqmusic@gmail.com>
2022-01-13 15:42:23 -0700

    added a lets go brandon page

--- app.py
+++ app.py
@@ -16,5 +16,9 @@ else:
 def home():
     return render_template('home.html')
 
+@app.route("/brandon")
+def brandon():
+    return render_template('brandon.html')
+
 #if __name__ == "__main__":
 #    app.run(host='0.0.0.0', debug=True)
